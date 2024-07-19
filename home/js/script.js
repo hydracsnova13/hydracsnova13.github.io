cbfdc819946010fcd4da68e9c1c2fcac4b4dd6cc
Debanjan Chakraborty <hydracsnova@gmail.com>
2024-07-20 04:53:31 +0530

    Add light/dark mode toggle with icons and enhanced design

--- home/js/script.js
+++ home/js/script.js
@@ -1,3 +1,9 @@
 document.getElementById('toggle-theme').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+    const themeIcon = document.getElementById('theme-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.src = '../icons/moon.svg';
+    } else {
+        themeIcon.src = '../icons/sun.svg';
+    }
 });
