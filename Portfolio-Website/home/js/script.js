1e1f9275e15122ef9e134561166bdc3c4035bcd8
Debanjan Chakraborty <hydracsnova@gmail.com>
2024-07-20 18:16:43 +0530

    Update color scheme and structure

--- Portfolio-Website/home/js/script.js
+++ Portfolio-Website/home/js/script.js
@@ -1,9 +1,29 @@
+// Function to apply the theme based on localStorage value
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.getElementById('theme-icon').src = '../icons/moon.svg';
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.getElementById('theme-icon').src = '../icons/sun.svg';
+    }
+}
+
+// Event listener for theme toggle button
 document.getElementById('toggle-theme').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.toggle('dark-mode');
     const themeIcon = document.getElementById('theme-icon');
-    if (document.body.classList.contains('dark-mode')) {
+    if (isDarkMode) {
         themeIcon.src = '../icons/moon.svg';
+        localStorage.setItem('theme', 'dark');
     } else {
         themeIcon.src = '../icons/sun.svg';
+        localStorage.setItem('theme', 'light');
     }
+});
+
+// Apply the theme on page load based on localStorage value
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    applyTheme(savedTheme);
 });
